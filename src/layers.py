8768da1fdb7f9a3ccf53fe0e567562236927637a
Dmytro Androsov <androsovdmitry80@gmail.com>
2022-05-16 03:25:53 +0300

    Update layers.py

--- src/layers.py
+++ src/layers.py
@@ -32,8 +32,8 @@ class NeighborEmbedding(tf.keras.layers.Embedding):
         return dict(list(base_config.items()) + list(config.items()))
 
     def _get_aggregated_neighbor_embeddings(self, token: tf.Tensor) -> tf.Tensor:
-        neighbor_index = tf.reshape(
-                    tf.gather(self._A.indices, tf.where(self._A.indices[:,0] == token))[:,:,1], (-1,) # second index is neighbor index
+        neighbor_index = tf.squeeze(
+                    tf.gather(self._A.indices, tf.where(self._A.indices[:,0] == token))[:,:,1] # second index is neighbor index
                     )
         neighbor_embeddings = embedding_ops.embedding_lookup_v2(self.embeddings, neighbor_index)
         return tf.reduce_sum(neighbor_embeddings, axis=0)
